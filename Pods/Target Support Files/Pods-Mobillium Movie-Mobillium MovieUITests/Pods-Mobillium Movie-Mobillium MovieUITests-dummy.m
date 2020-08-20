00d32d74739f0ae0c192e4843510780f32569b94
Mehmet Tarhan <memtarhan@gmail.com>
2020-08-20 22:00:24 +0300

    Downloaded Swinject, Alamofire and Kingfisher

---
+++ Pods/Target Support Files/Pods-Mobillium Movie-Mobillium MovieUITests/Pods-Mobillium Movie-Mobillium MovieUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mobillium_Movie_Mobillium_MovieUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Mobillium_Movie_Mobillium_MovieUITests
+@end
---
+++ Pods/Target Support Files/Pods-Mobillium MovieTests/Pods-Mobillium MovieTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mobillium_MovieTests : NSObject
+@end
+@implementation PodsDummy_Pods_Mobillium_MovieTests
+@end
